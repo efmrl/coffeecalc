be972a43ec00aad04f007720a628f5ebe32dd48d
Sam Falkner <sam.falkner@gmail.com>
2020-11-04 06:38:29 -0700

    titles and favicons

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,15 @@
 </script>
 
 <style>
-  .world {
-    @apply text-teal-500 italic; /*  here's some tailwind apply */
+  .container {
+    @apply flex flex-col h-full mx-auto max-w-5xl text-coffee-diva;
+  }
+  .pictured {
+    @apply bg-center bg-fixed bg-no-repeat bg-cover;
   }
 </style>
 
-<div class="flex flex-col h-full">
+<div class="container pictured bg-coffee-dark">
   <h1 class="text-center text-3xl"> coffeecalc </h1>
 
   <WaterBeans />
