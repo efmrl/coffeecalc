d702f423813f5dffa96a6a81d6f608cd34093f3c
Sam Falkner <sam.falkner@gmail.com>
2021-04-01 07:30:17 -0600

    one can edit the ratio, crudely

--- src/WaterBeans.svelte
+++ src/WaterBeans.svelte
@@ -1,9 +1,12 @@
 <script>
   import { CoffeeCalc } from "./coffee.js";
   import Arrows16 from "carbon-icons-svelte/lib/Arrows16";
+  import { fly } from "svelte/transition";
 
   const calc = new CoffeeCalc();
   let ccInput = 16;
+
+  let editRatio = false;
 
   function focus(e) {
     e.focus();
@@ -27,6 +30,33 @@
 </script>
 
 <div class="flex-grow grid grid-cols-2 content-start gap-2 mt-4 mx-8">
+  <section class="words place-self-center col-span-2">
+    {#if editRatio}
+      <span transition:fly={{ x: -256, duration: 200 }}>
+        <input
+          type="text"
+          size="3"
+          class="words text-right"
+          on:blur={() => {
+            editRatio = false;
+          }}
+          bind:value={calc.ratio}
+        />
+      </span>
+    {:else}
+      <span class="words" on:click={() => {editRatio = true}}>
+        {calc.ratio}
+      </span>
+    {/if}
+    <span
+      class="clicky"
+      on:click={() => {
+        editRatio = !editRatio;
+      }}
+    >
+      :1
+    </span>
+  </section>
   <label for="water" class="text-right">
     <span class="words" on:click={inputType}
       >{calc.inputTitle(ccInput)} in
